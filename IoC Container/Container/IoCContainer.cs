4b06bf23a323d2e161e52d6638b2ff0d8c28c63b
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-19 02:31:31 +0300

    adding more feature to the IoC container - adding the resolve feature , to create instance from the provided contract/interface - using the reflection to create instance of the provided contract/interface

--- IoC Container/Container/IoCContainer.cs
+++ IoC Container/Container/IoCContainer.cs
@@ -11,10 +11,31 @@ public class IoCContainer
     Dictionary<Type , Type> _map = new Dictionary<Type , Type>();
 
     public void Register<TContract, TImplementation>()
+        where TImplementation : class , TContract
+        where TContract : class
     {
         if (!_map.ContainsKey(typeof(TContract)))
         {
             _map.Add(typeof(TContract) , typeof(TImplementation));
         }
+    }
+
+    public TContract Resolve<TContract>()
+        where TContract : class
+    {
+        if (!_map.ContainsKey(typeof(TContract)))
+        {
+            throw new ArgumentException($"No Registration found for {typeof(TContract)}");
+        }
+
+        var implementation = _map[typeof(TContract)];
+
+        return Create<TContract>(implementation);
+    }
+
+    private TContract Create<TContract>(Type implementationType)
+        where TContract : class
+    {
+        return Activator.CreateInstance(implementationType, null) as TContract;
     }
 }
