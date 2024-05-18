3cee3ee0060b2c701c97c73d021867c94aa3aebe
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-19 02:15:48 +0300

    Registering Types building IoC with a register method to register a contarct/interface and it's implementation

---
+++ IoC Container/Container/IoCContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC_Container.Container;
+
+public class IoCContainer
+{
+    Dictionary<Type , Type> _map = new Dictionary<Type , Type>();
+
+    public void Register<TContract, TImplementation>()
+    {
+        if (!_map.ContainsKey(typeof(TContract)))
+        {
+            _map.Add(typeof(TContract) , typeof(TImplementation));
+        }
+    }
+}
