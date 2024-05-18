49d556cd69a906ef0e93b14f24f75fd2ccfe4796
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-05-19 02:58:53 +0300

    testing the container to provide instances

--- IoC Container/Program.cs
+++ IoC Container/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace IoC_Container;
+﻿using IoC_Container.Container;
+using IoC_Container.Demo;
+
+namespace IoC_Container;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        var iocContainer = new IoCContainer();
+
+        iocContainer.Register<IWaterService , TapWaterService>();
+        var waterService = iocContainer.Resolve<IWaterService>();
+
+        iocContainer.Register<ICoffeeService , CoffeeService>();
+        var coffeeService = iocContainer.Resolve<ICoffeeService>();
+
+        Console.WriteLine(waterService.GetType());
+        Console.WriteLine(coffeeService.GetType());
     }
 }
